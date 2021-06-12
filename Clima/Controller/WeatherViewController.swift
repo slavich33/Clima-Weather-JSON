76fde60e651a995535f0872f739dc272dd321f83
Slava Chu <slavich33@icloud.com>
2021-06-12 23:04:00 +0300

    Last version

---
+++ Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Angela Yu on 01/09/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class WeatherViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    var weatherManager = WeatherManager()
+    let locationmanager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationmanager.delegate = self
+        locationmanager.requestWhenInUseAuthorization()
+        locationmanager.requestLocation()
+        
+        searchTextField.delegate = self
+        weatherManager.delegate = self
+      
+    }
+    
+    @IBAction func myLocationButton(_ sender: UIButton) {
+
+        locationmanager.requestLocation()
+        //        if let location = locationmanager.location {
+//            let lat = location.coordinate.latitude
+//            let lon = location.coordinate.longitude
+//            weatherManager.fetchWeather(latitude: lat, longitude: lon)
+//        }
+    }
+}
+//MARK: - UITextFieldDelegate
+
+extension WeatherViewController: UITextFieldDelegate {
+    @IBAction func searchPressed(_ sender: UIButton) {
+        print(searchTextField.text!)
+        searchTextField.endEditing(true)
+    }
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(searchTextField.text!)
+        searchTextField.endEditing(true)
+        return true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let city = searchTextField.text {
+            weatherManager.fetchWeather(cityName: city) }
+        searchTextField.text = ""
+    }
+}
+
+//MARK: - WeatherManagerDelegate
+
+extension WeatherViewController: WeatherManagerDelegate {
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+            self.cityLabel.text = "\(weather.cityName)"
+        }
+    }
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+}
+
+//MARK: - CLLocation
+
+extension WeatherViewController: CLLocationManagerDelegate {
+
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            locationmanager.stopUpdatingLocation()
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            weatherManager.fetchWeather(latitude: lat, longitude: lon)
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    }
+    
+
---
+++ Clima/WeatherData.swift
@@ -0,0 +1,22 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Slava on 21.02.2021.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+   let temp: Double
+}
+struct Weather: Codable {
+    let id: Int
+}
